2ba1066039461a1477d4068a6d21a5fd69765d99
RedEyesNCode <ashutosh.singh19071999@gmail.com>
2022-06-20 23:28:43 +0530

    Update Readme & design dashboard Screen

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # PixelPerfectXML
 This Repo recalls the ways to acheive Pixel-Perfect UI in the XML of Android.
+
+-This Repository will contain different showcasing different UI's flow if different apps like Social Media, Food Apps and More..
+<table>
+    <tr>
+    <td>Pixel Perfect XML</td>
+    </tr>
+    <tr>
+    <td><img src="media/pixel_perfect.png"></td>
+    </tr>
+</table>
--- app/src/main/java/com/redeyesncode/cleancodexml/auth/LoginActivity.kt
+++ app/src/main/java/com/redeyesncode/cleancodexml/auth/LoginActivity.kt
@@ -1,7 +1,9 @@
 package com.redeyesncode.cleancodexml.auth
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.redeyesncode.cleancodexml.DashboardActivity
 import com.redeyesncode.cleancodexml.R
 import com.redeyesncode.cleancodexml.databinding.ActivityLoginBinding
 
@@ -9,8 +11,12 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        var binding:ActivityLoginBinding
+        var binding:ActivityLoginBinding = ActivityLoginBinding.inflate(layoutInflater)
+        binding.btnNext.setOnClickListener {
+            var intentNext = Intent(this@LoginActivity,DashboardActivity::class.java)
+            startActivity(intentNext)
+        }
 
-        setContentView(R.layout.activity_login)
+        setContentView(binding.root)
     }
 }
