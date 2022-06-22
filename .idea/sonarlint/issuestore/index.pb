511e3c2158b71405d9464e211a52ded46fd6cd95
RedEyesNCode <ashutosh.singh19071999@gmail.com>
2022-06-22 12:12:45 +0530

    Update Designs

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,9 @@
+
+l
+<app/src/main/java/com/redeyesncode/uberclone/MainActivity.kt,e\9\e9d683d79e272530daa4d712ec13ecf649985afa
+@
+app/build.gradle,f\4\f4a01d6a4fcb971362ec00a83903fd3902f52164
+9
+	README.md,8\e\8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+P
+ app/src/main/AndroidManifest.xml,8\c\8c55c3ccc257e5907959013f99656e4c8ec3903e
--- app/src/main/java/com/redeyesncode/uberclone/LoginActivity.kt
+++ app/src/main/java/com/redeyesncode/uberclone/LoginActivity.kt
@@ -1,11 +1,25 @@
 package com.redeyesncode.uberclone
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.inputmethod.EditorInfo
+import com.redeyesncode.uberclone.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        binding.edtNumber.setOnEditorActionListener { v, actionId, event ->
+            if(actionId == EditorInfo.IME_ACTION_DONE){
+                var intent = Intent(this@LoginActivity,OtpActivity::class.java)
+                startActivity(intent)
+                 true
+            } else {
+                false
+            }
+        }
+        setContentView(binding.root)
     }
 }
